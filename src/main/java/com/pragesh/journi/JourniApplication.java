3a59d94bb3e88f65b36ab518d6ed03296f0d7c10
prageshb <bhandaripragesh@gmail.com>
2024-10-23 14:15:51 +0545

    Initial Upload

---
+++ src/main/java/com/pragesh/journi/JourniApplication.java
@@ -0,0 +1,13 @@
+package com.pragesh.journi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragesh/journi/JourniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragesh.journi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
